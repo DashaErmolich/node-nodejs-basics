e1e5e9c520ee966cc9d3e73e7ad5a09aa6056db5
DashaErmolich <dasha.ermolich@gmail.com>
2024-01-31 16:47:40 +0100

    feat: implement worker threads functions

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,34 @@
+import path from "path";
+import url from "url";
+import os from "os";
+import { Worker } from "worker_threads";
+
 const performCalculations = async () => {
-    // Write your code here
+  const fileName = "worker.js";
+
+  const __filename = url.fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+
+  let i = 10;
+
+  const allPromises = os.cpus().map(() => {
+    return new Promise((resolve, reject) => {
+      const worker = new Worker(path.resolve(__dirname, fileName), {
+        workerData: i++,
+      });
+      worker.on("message", (value) => resolve(value));
+      worker.on("error", (value) => reject(value));
+    });
+  });
+
+  const workerResult = await Promise.allSettled(allPromises);
+
+  const result = workerResult.map(({ status, value }) => ({
+    status: status === "rejected" ? "error" : "resolved",
+    data: status === "rejected" ? null : value,
+  }));
+
+  console.log(result);
 };
 
 await performCalculations();
