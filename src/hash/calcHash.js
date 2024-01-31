24093f1cb1860ad1452dcc8953c230c022374031
DashaErmolich <dasha.ermolich@gmail.com>
2024-01-31 15:28:26 +0100

    feat: implement calculateHash function

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,22 @@
+import crypto from 'crypto';
+import fsp from "fs/promises";
+import path from "path";
+import url from "url";
+
 const calculateHash = async () => {
-    // Write your code here 
+    const folderName = "files";
+    const fileName = "fileToCalculateHashFor.txt";
+
+    const __filename = url.fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+
+    const fileBuffer = await fsp.readFile(path.resolve(__dirname, folderName, fileName));
+    const hashSum = crypto.createHash('sha256');
+    hashSum.update(fileBuffer)
+
+    const hex = hashSum.digest('hex');
+
+    console.log(hex);
 };
 
 await calculateHash();
