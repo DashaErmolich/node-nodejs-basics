ace4d96053b22e622b282703da7dddb46cfa790d
DashaErmolich <dasha.ermolich@gmail.com>
2024-01-31 14:04:49 +0100

    feat: implement parseEnv function

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,7 @@
 const parseEnv = () => {
-    // Write your code here 
+  const dataArr = Object.entries(process.env);
+  const result = dataArr.map((data) => `RSS_${data[0]}=${data[1]}`);
+  console.log(result.join('; '));
 };
 
 parseEnv();
