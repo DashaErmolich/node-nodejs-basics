fdac2f05bc6087975ba67b0ce46eb0c4517a5046
DashaErmolich <dasha.ermolich@gmail.com>
2024-01-31 13:54:54 +0100

    feat: implement read function

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,29 @@
+import fsp from "fs/promises";
+import path from "path";
+import url from "url";
+
 const read = async () => {
-    // Write your code here 
+  const folderName = "files";
+  const readFileName = "fileToRead.txt";
+
+  let errorMessage = "FS operation failed";
+
+  const __filename = url.fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+
+  try {
+    const content = await fsp.readFile(
+      path.resolve(__dirname, folderName, readFileName),
+      { encoding: "utf8" }
+    );
+    console.log(content);
+  } catch (error) {
+    if (error.code !== "ENOENT") {
+      errorMessage = error.message;
+    }
+
+    throw new Error(errorMessage);
+  }
 };
 
 await read();
