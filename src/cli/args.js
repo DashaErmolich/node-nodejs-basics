6f5a9a6582fe2bf5aadfaa51f6f962e913861e89
DashaErmolich <dasha.ermolich@gmail.com>
2024-01-31 14:38:44 +0100

    feat: implement parseArgs function

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,14 @@
 const parseArgs = () => {
-    // Write your code here 
+  const dataArr = process.argv.slice(2);
+  const result = [];
+
+  for (let i = 0; i < dataArr.length; i += 2) {
+    if (dataArr[i].startsWith('--')) {
+        result.push(`${dataArr[i].slice(2)} is ${dataArr[i + 1]}`)
+    }
+  }
+
+  console.log(result.join(", "));
 };
 
 parseArgs();
