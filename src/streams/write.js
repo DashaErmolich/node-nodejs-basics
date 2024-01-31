f46947b0818b6b26cf47af43a4ac184966efbc4f
DashaErmolich <dasha.ermolich@gmail.com>
2024-01-31 15:45:35 +0100

    feat: implement write function

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,18 @@
+import fs from 'fs';
+import process from 'process';
+import path from "path";
+import url from "url";
+
 const write = async () => {
-    // Write your code here 
+    const folderName = "files";
+    const fileName = "fileToWrite.txt";
+
+    const __filename = url.fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+
+    const writeStream = fs.createWriteStream(path.resolve(__dirname, folderName, fileName));
+
+    process.stdin.pipe(writeStream);
 };
 
 await write();
