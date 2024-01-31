e7621e62819ce4a15987451a299996ee44a1fbdd
DashaErmolich <dasha.ermolich@gmail.com>
2024-01-31 16:00:04 +0100

    feat: implement transform function

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,18 @@
+import process from "process";
+import stream from "stream";
+
 const transform = async () => {
-    // Write your code here 
+  const reverseInput = new stream.Transform({
+    transform(chunk, encoding, callback) {
+      callback(null, String(chunk).split("").reverse().join(""));
+    },
+  });
+
+  stream.pipeline(process.stdin, reverseInput, process.stdout, (err) => {
+    if (err) {
+      console.log(err.message);
+    }
+  });
 };
 
 await transform();
