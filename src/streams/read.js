60b24558ae9ec5ff59fae993f7ca440016ee7961
DashaErmolich <dasha.ermolich@gmail.com>
2024-01-31 15:42:32 +0100

    feat: implement read function

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,18 @@
+import fs from 'fs';
+import process from 'process';
+import path from "path";
+import url from "url";
+
 const read = async () => {
-    // Write your code here 
+    const folderName = "files";
+    const fileName = "fileToRead.txt";
+
+    const __filename = url.fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+
+    const readStream = fs.createReadStream(path.resolve(__dirname, folderName, fileName));
+
+    readStream.pipe(process.stdout);
 };
 
 await read();
